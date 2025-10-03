12a50116140fd10f82d3c17699227e9aa02ba4bd
zagob <matheusbestana@gmail.com>
2025-10-03 15:52:47 -0300

    Implement database setup

---
+++ src/database/mongoose.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+  cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+  if (!MONGODB_URI) throw new Error("Missing MONGODB_URI");
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;   
+  } catch (error) {
+    cached.promise = null
+    throw error
+  }
+
+  console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
+};
